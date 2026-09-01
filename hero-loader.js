1b484d3ac44c353c2f66a921724690b4f7055888
Jemobulas <wayfjemobulas@gmail.com>
2026-09-01 16:12:43 +0300

    .

--- hero-loader.js
+++ hero-loader.js
@@ -248,10 +248,12 @@ function buildSkillBuilds(builds, heroPositions) {
     }).join('');
 
     return `<div class="build-card">` +
-      `<h4>${build.name}</h4>` +
-      `<div class="build-body"><div class="build-spells">${tsHeader}${rowsHTML}</div></div>` +
-      `<div class="talent-tree">${talentsHTML}</div>` +
-      (build.talent_note ? `<div class="talent-note-label">${build.talent_note}</div>` : '') +
+      `<div class="build-inner">` +
+        `<h4>${build.name}</h4>` +
+        `<div class="build-body"><div class="build-spells">${tsHeader}${rowsHTML}</div></div>` +
+        `<div class="talent-tree">${talentsHTML}</div>` +
+        (build.talent_note ? `<div class="talent-note-label">${build.talent_note}</div>` : '') +
+      `</div>` +
       `<div class="build-note">${build.note}</div>` +
     `</div>`;
   }).join('');
@@ -279,8 +281,10 @@ function buildItemBuilds(builds, situational) {
     }).join('');
 
     return `<div class="build-card">` +
-      `<h4>${build.name}</h4>` +
-      phasesHTML +
+      `<div class="build-inner">` +
+        `<h4>${build.name}</h4>` +
+        phasesHTML +
+      `</div>` +
       `<div class="build-note">${build.note}</div>` +
     `</div>`;
   }).join('');
